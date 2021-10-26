c441972eca1eaa583b90bbb7921ee98a6eaa1d45
tenmasato <narito1015@icloud.com>
2021-10-26 05:41:42 +0000

    Update

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :books
+  resources :users,only: [:show,:index,:edit,:update]
+
+
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
+end

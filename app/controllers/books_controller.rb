783449e662e7b14e3325a4dedfb73fce96871420
tenmasato <narito1015@icloud.com>
2021-10-26 08:34:43 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -35,16 +37,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 
 end
